61ca694479802f190cb304141f39742caf54ae01
Paulo Sousa <seuemail@gmail.com>
2026-06-04 18:58:03 -0300

    criando o projeto Spring Boot inicial

---
+++ social-media-manager/src/main/java/com/paulofelipe/social_media_manager/SocialMediaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.paulofelipe.social_media_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaManagerApplication.class, args);
+	}
+
+}
---
+++ social-media-manager/src/test/java/com/paulofelipe/social_media_manager/SocialMediaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulofelipe.social_media_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
